a46990aa37cf2da422ba0c44501e6b98ad585705
Abdul Muqeet Arshad <imuqeetchaudhary@gmail.com>
2022-02-07 19:29:33 +0500

    added Carousel.js

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,28 +1,26 @@
-import img1 from "./img/slide-img-1.jpg";
-import img2 from "./img/slide-img-2.jpg";
-import img3 from "./img/slide-img-3.jpg";
+import img1 from './img/slide-img-1.jpg';
+import img2 from './img/slide-img-2.jpg';
+import img3 from './img/slide-img-3.jpg';
 
-
-const detail =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit ecommerce.";
+const detail = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit ecommerce.';
 
 export const carouselArray = [
-  {
-    id: 1,
-    img: img1,
-    title: "First Slide",
-    detail,
-  },
-  {
-    id: 2,
-    img: img2,
-    title: "Second Slide",
-    detail,
-  },
-  {
-    id: 3,
-    img: img3,
-    title: "Third Slide",
-    detail,
-  },
+	{
+		id: 1,
+		img: img1,
+		title: 'First Slide',
+		detail,
+	},
+	{
+		id: 2,
+		img: img2,
+		title: 'Second Slide',
+		detail,
+	},
+	{
+		id: 3,
+		img: img3,
+		title: 'Third Slide',
+		detail,
+	},
 ];
